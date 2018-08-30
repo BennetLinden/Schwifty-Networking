a7f17d9c12931ceae17de4c5e364db7cb0e00ec4
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2018-08-30 16:42:33 +0200

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Schwifty-Networking' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'SwiftLint'
+  pod 'Alamofire', '~> 4.7'
+  pod 'PromiseKit', '~> 6.4'
+  pod 'RxCocoa', '~> 4.2'
+  pod 'RxSwift', '~> 4.2'
+  pod 'RxAlamofire', '~> 4.2'
+
+end
--- Schwifty-Networking/AppDelegate.swift
+++ Schwifty-Networking/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
         return true
     }
 
@@ -41,6 +46,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
