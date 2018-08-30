0a092ad2da3b34f1b35519d9a7f0aeaf1e0db564
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2018-08-30 16:28:44 +0200

    Initial Commit

---
+++ Schwifty-Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schwifty-Networking
+//
+//  Created by Bennet van der Linden on 30/08/2018.
+//  Copyright © 2018 Schwifty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
